9d56a14ef470ed16cca4604a6a506e97df8e4deb
Chris <rocknollnightmare@web.de>
2018-06-15 11:17:11 +0200

    Add inputText to connect to mongoDB and update chart

--- app.js
+++ app.js
@@ -3,26 +3,35 @@ require('./config/config');
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const moment = require('moment');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const {mongoose} = require('./server/mongoose');
-const {InsertCountries} = require('./db/InsertData');
-const {Daten} = require('./models/daten');
+
+const { mongoose } = require('./server/mongoose');
+const { InsertCountries } = require('./db/InsertData');
+const { Daten } = require('./models/daten');
 
 
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 
 app.set('view engine', 'hbs');
 InsertCountries();
 
 app.get('/data.json', (req, res) => {
-    Daten.find().then((countries) => { 
-    res.send(countries);
-    
-}, (e) => {
-    res.status(400).send(e);
-})
+    Daten.find().then((countries) => {
+        res.send(countries);
+
+    }, (e) => {
+        res.status(400).send(e);
+    })
 });
+
+
 
 
 app.use((req, res, next) => {
@@ -30,7 +39,7 @@ app.use((req, res, next) => {
     let log = `${now}: ${req.method} ${req.url}`;
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err) => {
-        if(err) {
+        if (err) {
             console.log('Unable to append to server.log.')
         }
     })
@@ -40,12 +49,31 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/node_modules/amcharts3/amcharts'));
 
+io.on('connection', (socket) => {
+    console.log('Connected');
+
+    socket.on('addData', (data, callback) => {
+        console.log(data);
+        Daten.insertMany(data);
+
+        app.get('/data.json', (req, res) => {
+            Daten.find().then((countries) => {
+                res.send(countries);
+
+            });
+        });
+        callback();
+    });
+});
+
+
 app.get('/', (req, res) => {
     res.render('home.hbs');
 });
 
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 });
--- views/scripts/main.js
+++ views/scripts/main.js
@@ -1,7 +1,7 @@
+let socket = io();
 
-  
-  
-AmCharts.makeChart( "chartdiv", {
+
+AmCharts.makeChart("chartdiv", {
   "type": "serial",
   "dataLoader": {
     "url": "http://localhost:3000/data.json",
@@ -9,8 +9,43 @@ AmCharts.makeChart( "chartdiv", {
     "async": true
   },
   "categoryField": "country",
-  "graphs": [ {
+  "graphs": [{
     "valueField": "visits",
     "type": "column"
-  } ]
-} );
+  }]
+});
+
+jQuery(document).ready(function () {
+  jQuery('#chart-input').on('submit', function (e) {
+    e.preventDefault();
+
+    let countryTextbox = jQuery('[name=country]');
+    let visitsTextbox = jQuery('[name=visits]');
+
+
+    socket.emit('addData', {
+      "country": countryTextbox.val(),
+      "visits": parseInt(visitsTextbox.val())
+    }, function () {
+      countryTextbox.val('');
+      visitsTextbox.val('');
+    });
+
+    AmCharts.makeChart("chartdiv", {
+      "type": "serial",
+      "dataLoader": {
+        "url": "http://localhost:3000/data.json",
+        "format": "json",
+        "async": true
+      },
+      "categoryField": "country",
+      "graphs": [{
+        "valueField": "visits",
+        "type": "column"
+      }]
+    });
+
+  });
+});
+
+
